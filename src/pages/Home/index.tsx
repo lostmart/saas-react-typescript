bcba85d002d3e3096f83e4bff386a86f8fae8e26
lostmart <lostmart@hotmail.com>
2023-04-14 22:01:33 +0200

    inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,15 @@
-import { FaEnvelope } from 'react-icons/fa'
+import { useState } from 'react'
+import { FaEnvelope, FaEye, FaEyeSlash } from 'react-icons/fa'
 import logoImg from '../../assets/logo-round.png'
 import styles from './index.module.css'
 
 const Home = () => {
+	const [showPassword, togglePassword] = useState(true)
+
+	const handleClick = (): void => {
+		togglePassword(!showPassword)
+	}
+
 	return (
 		<div>
 			<div className={styles.grey_circle}>
@@ -21,10 +28,29 @@ const Home = () => {
 			</div>
 			<form className="form">
 				<div className="form_control">
-					<FaEnvelope className='form_inputIcon' />
+					<FaEnvelope className="form_inputIcon" />
 
 					<input type="text" placeholder="Email" className="form_email" />
 				</div>
+
+				<div className="form_control">
+					{!showPassword ? (
+						<FaEye className="form_inputIcon" onClick={handleClick} />
+					) : (
+						<FaEyeSlash className="form_inputIcon" onClick={handleClick} />
+					)}
+
+					<input
+						type={showPassword ? 'password' : 'text'}
+						placeholder="Password"
+						className="form_email"
+					/>
+				</div>
+
+				<div className="form_control">
+					<button className="form_email btn btn-primary">Connect</button>
+				</div>
+				<p>Forgot stupid password?</p>
 			</form>
 		</div>
 	)
