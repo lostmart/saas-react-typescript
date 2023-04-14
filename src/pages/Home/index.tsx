f664073c3db74d69a4220d134fa8833496cf08ff
lostmart <lostmart@hotmail.com>
2023-04-14 20:39:17 +0200

    inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,31 @@
+import { FaEnvelope } from 'react-icons/fa'
+import logoImg from '../../assets/logo-round.png'
+import styles from './index.module.css'
+
 const Home = () => {
 	return (
 		<div>
-			<p>hello</p>
-			<div className="grey-circle">
+			<div className={styles.grey_circle}>
 				<div className="mobile-phone">
 					<div className="speaker"></div>
-					<div className="screen"></div>
+					<div className="screen">
+						<img src={logoImg} className={styles.logo} alt="home school" />
+						<h2 className={styles.title}>
+							Learn <br />
+							@ <br />
+							Home
+						</h2>
+					</div>
 					<div className="microphone"></div>
 				</div>
 			</div>
+			<form className="form">
+				<div className="form_control">
+					<FaEnvelope className='form_inputIcon' />
+
+					<input type="text" placeholder="Email" className="form_email" />
+				</div>
+			</form>
 		</div>
 	)
 }
